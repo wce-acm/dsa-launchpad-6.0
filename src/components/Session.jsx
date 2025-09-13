1f96fce3f34995e17dea1a6bb0d1c8d67fd26f41
PranavKulkarni581 <pranavkulkarni581@gmail.com>
2025-09-13 12:16:09 +0530

    session commit

--- src/components/Session.jsx
+++ src/components/Session.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import HarryPotterCard from "./Card";
 import Particles from "./ui/Particles";
 
+import one1 from "../assets/images/one1.png";
+import two from "../assets/images/two.png";
+import three from "../assets/images/three.png";
+import four from "../assets/images/four.png";
+
 const SessionCard = () => {
-  const cards = [1, 2, 3, 4]; // you can extend this array
+  const cards = [1, 2, 3, 4];
+  const logos = [one1, two, three, four]; 
 
   return (
-
     <div className="relative flex flex-col items-center">
-         <div className="absolute inset-0 z-10">
+      {/* Background Particles */}
+      <div className="absolute inset-0 z-10">
         <Particles
           particleColors={["#ffffff", "#f6f4f1", "#f3f1ea"]}
           particleCount={300}
@@ -20,25 +26,36 @@ const SessionCard = () => {
           disableRotation={false}
         />
       </div>
+
       {/* Snake line */}
       <div className="absolute left-1/2 top-0 bottom-0 w-1 bg-amber-800 -translate-x-1/2 z-0" />
 
-      {cards.map((card, index) => (
-        <div
-          key={index}
-          className={`relative flex w-full max-w-5xl my-8 z-10 ${
-            index % 2 === 0 ? "justify-start" : "justify-end"
-          }`}
-        >
-          {/* Connector circle */}
+      {cards.map((card, index) => {
+        const isLeft = index % 2 === 0; // even index = left card
+        return (
           <div
-            className="absolute top-1/2 left-1/2 w-6 h-6 bg-amber-600 border-4 border-amber-900 rounded-full -translate-x-1/2 -translate-y-1/2 z-20"
-          />
+            key={index}
+            className={`relative flex w-full max-w-5xl my-16 z-10 ${
+              isLeft ? "justify-start" : "justify-end"
+            }`}
+          >
+            {/* Connector circle */}
+            <div className="absolute top-1/2 left-1/2 w-6 h-6 bg-amber-600 border-4 border-amber-900 rounded-full -translate-x-1/2 -translate-y-1/2 z-20" />
 
-          {/* Card itself */}
-          <HarryPotterCard />
-        </div>
-      ))}
+            {/* The card itself */}
+            <HarryPotterCard />
+
+            {/* The logo (absolute, opposite to card) */}
+            <img
+              src={logos[index]}
+              alt={`Logo ${index + 1}`}
+              className={`w-45 h-45 object-contain absolute top-1/2 -translate-y-1/2 float-animation ${
+                isLeft ? "right-[560px]" : "left-[560px]"
+              }`}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
