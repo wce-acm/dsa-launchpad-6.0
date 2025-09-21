f0c4c8b515b60465151d25e08823e6af28175688
acw-acm <wceacmsc@gmail.com>
2025-09-21 12:25:55 +0530

    background

--- frontend/src/components/Background.jsx
+++ frontend/src/components/Background.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import bgImage from "../assets/images/background-image.jpg"; // ✅ import image
 
 const Background = ({ children }) => {
   return (
     <div className="relative w-full min-h-screen">
-      {/* Background layer */}
-      <div className="fixed top-0 left-0 w-full h-full min-h-screen bg-black gradient-container -z-10" />
+      {/* Background image */}
+      <div
+        className="fixed top-0 left-0 w-full h-full -z-20 bg-cover bg-center bg-fixed"
+        style={{
+          backgroundImage: `url(${bgImage})`,
+        }}
+      />
+
+      {/* Black overlay with transparency */}
+      <div className="fixed top-0 left-0 w-full h-full bg-black opacity-60 -z-10" />
 
       {/* Foreground content */}
       <div className="relative z-10">
